aacd21d4204794fa7c48fd9d69d840c718c208c2
Piyush Sukhwani <piyushsukhwani7@gmail.com>
2025-03-22 21:38:56 +0530

    Added Configuration Class

---
+++ src/test/java/com/hirrd/HirrdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
